01e53848b3e08fa9730be29204cb8bd5a6818f5e
Mohan <mohan-developer>
2018-09-19 18:49:15 +0530

    education template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+const menuRouts: Routes = [
+  {path : '', component : HomeComponent},
+  {path : 'about', component : AboutComponent},
+  {path : 'blog', component : BlogComponent},
+  {path : 'gallery', component : GalleryComponent},
+  {path : 'contacts', component : ContactsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactsComponent,
+    GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(menuRouts,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
